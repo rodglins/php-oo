adbac0a0b7467aa8c1dd9237ca551edd5f734529
Rodrigo Aglinskas <rodglins@gmail.com>
2021-07-07 17:54:57 -0300

    Criação de serviços e funções PHP

---
+++ php-intro/script.php
@@ -0,0 +1,12 @@
+<?php 
+
+include "servicos/servicoMensagemSessao.php";
+include "servicos/servicoValidacao.php";
+include "servicos/servicoCategoriaCompetidor.php";
+
+$nome = $_POST['nome'];
+$idade = $_POST['idade'];
+
+defineCategoriaCompetidor($nome, $idade);
+
+header('location: index.php');
